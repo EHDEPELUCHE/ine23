e177980464deb94f3b21b0a8518dd8288a4bf325
Álvaro Pérez Vargas <80173076+EHDEPELUCHE@users.noreply.github.com>
2023-10-19 17:38:21 +0200

    Actualizar master.blade.php

--- resources/views/templates/master.blade.php
+++ resources/views/templates/master.blade.php
@@ -1,3 +1,30 @@
+
+<!doctype html>
+<!-- Bootstrap first template for Internet y Negocio Electrónico, University of Cadiz,
+     since 2019, based on https://www.w3schools.com/bootstrap4/bootstrap_templates.asp -->
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">      
+    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+    <!-- 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script> 
+    -->
+    
+    <link rel="shortcut icon" href="img/icon.jfif">
+    
+    <style>
+      .fakeimg { height: 100px; background: #aaa; }
+    </style>
+
+    <title>My INE project P1</title>
+  </head>
+<body>
+
+
 @include('layouts.header')
 
 @yield('content-center')
