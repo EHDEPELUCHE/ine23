9e37c117a871652c764c4ada2d92bc5f2027a265
elenavaz <elena.vazrodri@alum.uca.es>
2023-10-26 20:09:15 +0200

    colorsito

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,13 +23,14 @@
                         <button type="submit" class="btn btn-sm btn-outline-secondary">Ver Info</button>
                       </form>
                     </div>
-                       <small class="text-muted">                 
+                                       
                     @if($jueguito->HasDiscount())
-                    
-                      {{number_format($jueguito->DiscountApplied(), 2)}}
+                    <small class="text-muted"><font color="#DC143C">
+                      {{number_format($jueguito->DiscountApplied(), 2) . " €"}}
+                    </font>
                     @else
-                    
-                      {{$jueguito->price}}
+                    <small class="text-muted">
+                      {{$jueguito->price . " €"}}
                     @endif
 
                     </small>
