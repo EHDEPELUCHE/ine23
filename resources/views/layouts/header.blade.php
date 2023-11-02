102ee185790d5bf7acf5be6a722245bf277ced45
elenavaz <elena.vazrodri@alum.uca.es>
2023-11-02 20:24:38 +0100

    div bien puestos

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,23 +1,22 @@
 <!-- LAYOUT: HEADER -->
       <!-- SECTION: Main menu -->
-      <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="/"><img src="/ico/icon.jfif" width="50px" heigth="70px"></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar" >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="collapsibleNavbar">
-          <ul class="navbar-nav mr-auto me-auto mb-2 mb-md-0">
-            <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
-            <li class="nav-item"><a class="nav-link" href="#">Registrarse</a></li>
-            <li class="nav-item"><a class="nav-link" href="#">Iniciar Sesión</a></li>   
-          </ul>
-         
-          <form class="d-flex form-inline" role="search" METHOD= "GET" ACTION="buscador" >
-            <input class="form-control me-2" type="text" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-          </form>
-        </div>
-        </div>  
-        </div>
-      </nav>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/"><img src="/ico/icon.jfif" width="50px" heigth="70px"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar" >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <ul class="navbar-nav mr-auto me-auto mb-2 mb-md-0">
+        <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Registrarse</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Iniciar Sesión</a></li>   
+      </ul>
+      
+      <form class="d-flex form-inline" role="search" METHOD= "GET" ACTION="buscador" >
+        <input class="form-control me-2" type="text" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
